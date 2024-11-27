3ab3dabffef1b1152e6809b6bd87c935c745fef7
Ilan Attia <attia.ilan@gmail.com>
2024-11-27 14:39:18 +0100

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -80,6 +80,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_27_114312) do
     t.float "score"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "artwork_id"
   end
 
   create_table "games", force: :cascade do |t|

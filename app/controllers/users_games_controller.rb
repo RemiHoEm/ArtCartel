1c721c9ff470c7fd79e2d33e05abbd810c67d60e
Milena Arms <183767485+The-Milena@users.noreply.github.com>
2024-12-04 16:41:12 +0100

    leaderboard done

--- app/controllers/users_games_controller.rb
+++ app/controllers/users_games_controller.rb
@@ -2,6 +2,17 @@ class UsersGamesController < ApplicationController
 
   def show
     @users_game = UsersGame.find(params[:id])
+
+    @game_id = @users_game.game_id
+    puts "Its my number of the game #{@game_id}"
+
+    @players_scores = User.joins(users_games: :challenges)
+                          .where(users_games: { game_id: @game_id })
+                          .group('users.id', 'users.username') # Group by user to sum their scores
+                          .select('users.username, SUM(challenges.score) AS total_score')
+                          .order('total_score DESC') # Optional: order by total score
+
+    @ranking = 1
   end
 
   def create
